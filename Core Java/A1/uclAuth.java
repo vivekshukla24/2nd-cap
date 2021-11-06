973e4286cbf04cb69b3e7494e58f02ac2de3f1f9
Vivek Shukla <vivek-kumar.shukla@capgemini.com>
2021-11-06 13:00:57 +0530

    corrected the auth question

--- Core Java/A1/uclAuth.java
+++ Core Java/A1/uclAuth.java
@@ -6,6 +6,7 @@ public class auth {
         HashMap<String,Integer> user=new HashMap<String,Integer>();
         Scanner s= new Scanner(System.in);
         user.put("vivekshukla",12345);
+        user.put("newuser",45678);
         System.out.println("Enter your username");
         String username=s.next();
         int count=0;
@@ -17,7 +18,7 @@ public class auth {
             if(user.containsKey(username)){
                 System.out.println("Enter your password");
                 int p=s.nextInt();
-                if(user.containsValue(p)){
+                if(user.get(username)==p){
                     System.out.println("Welcome: "+username);
                     break;
                 }
