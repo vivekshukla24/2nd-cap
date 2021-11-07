b296e4967991df3d1080e7e6aebc803d03c88537
Vivek Shukla <vivek-kumar.shukla@capgemini.com>
2021-11-08 01:21:58 +0530

    adding second question of 5th assignment

---
+++ Core Java/A5/hashmap.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Random;
+
+public class nums {
+    public static void main(String[] args) {
+        HashMap<Integer,Double> store=new HashMap<Integer,Double>();
+        Random r=new Random();
+        int max=45;
+        double range=8.76;
+        double min=7;
+        for (int i = 0; i < 10; i++) {
+            int x=(int)(r.nextInt(max));
+            double y=range+r.nextDouble()-min;
+            store.put(x,y);
+            System.out.println(x+" "+y);
+        }
+        System.out.println(" ");
+        System.out.println(store);
+    }
+}
