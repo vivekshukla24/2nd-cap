9cce8cf4812ef3247f877dc6c6e2fde804288afd
Vivek Shukla <vivek-kumar.shukla@capgemini.com>
2021-11-10 17:15:56 +0530

    2nd q of anno

---
+++ Core Java/annotation/Q2custom.java
@@ -0,0 +1,15 @@
+@interface info{
+    int authorID();
+    String author() default "Null";
+    String supervisor() default "Null";
+    String date();
+    String time();
+    int version();
+    String description() default "General class";
+}
+
+
+@info(authorID = 12, date = "21 March", time = "4pm", version = 6)
+public class custom {
+
+}
