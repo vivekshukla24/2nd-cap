a53e0b6ffe461e291fa6828ac993e5c398f6bbf9
Vivek Shukla <vivek-kumar.shukla@capgemini.com>
2021-11-12 10:37:41 +0530

    commented

--- Core Java/lambda/Q6replace.java
+++ Core Java/lambda/Q6replace.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 public class Q6replace {
     public static void main(String[] args) {
         List<String> words = new ArrayList<>();
+        //adding words to the lists
         words.add("enough");
         words.add("india");
         words.add("this");
