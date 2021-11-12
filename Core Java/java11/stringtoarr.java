b526652be41b1ab9e17fdf14f0b757151b350d05
Vivek Shukla <vivek-kumar.shukla@capgemini.com>
2021-11-12 16:28:57 +0530

    add

--- Core Java/java11/stringtoarr.java
+++ Core Java/java11/stringtoarr.java
@@ -5,6 +5,7 @@ public class stringtoarr {
     public static void main(String[] args) {
         String str="A quick brown fox jumps over lazy dog";
         ArrayList<String> strarr=new ArrayList<>();
+        //this will divide the string recursivly when it encounters a space
         strarr.addAll(Arrays.asList(str.split(" ")));
         System.out.println(strarr);
         String[] array= strarr.toArray(new String[0]);
