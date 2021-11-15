d70b690f8620540de272df81080657550d69133a
Vivek Shukla <vivek-kumar.shukla@capgemini.com>
2021-11-15 14:51:15 +0530

    added trader class

---
+++ Core Java/StreamAPI/Trader.java
@@ -0,0 +1,29 @@
+public class Trader {
+    String name;
+    String city;
+    public Trader(String name, String city) {
+        super();
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Trader [name=" + name + ", city=" + city + "]";
+    }
+
+
+}
